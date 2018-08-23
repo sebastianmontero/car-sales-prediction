cf1d158c3f2ea765d1b865254fab907364bbc449
Sebastian <sebastian.montero@gmail.com>
2018-08-23 15:04:40 +0900

    Added experiment trainer

--- seb/prediction/ensemble_trainer.py
+++ seb/prediction/ensemble_trainer.py
@@ -46,6 +46,19 @@ register_trainable('car_sales_prediction_trainable', ModelTrainable)
 
 ensemble_trainer = EnsembleTrainer({
                 'line_id': 102,
+                'keep_prob' : grid_search([0.9, 1.0]),
+                'layer_0' : grid_search([26, 29, 31, 34, 37]),
+                'max_epoch' : 2,
+                'train_months': 36,
+                'prediction_size': 1,
+                'store_window' : False,
+                'included_features' : ['inflation_index_roc_prev_month',
+                                      'manufacturing_confidence_index'],
+                'num_steps': [12, 24]
+            }, repeats=4, description='platform_26_37')
+
+'''ensemble_trainer = EnsembleTrainer({
+                'line_id': 102,
                 'keep_prob' : grid_search([0.8, 0.9, 1.0]),
                 'layer_0' : grid_search([15, 20, 25, 30]),
                 'max_epoch' : 2,
@@ -55,8 +68,7 @@ ensemble_trainer = EnsembleTrainer({
                 'included_features' : ['inflation_index_roc_prev_month',
                                       'manufacturing_confidence_index'],
                 'num_steps': 12
-            }, repeats=4, description='platform_x5')
-
+            }, repeats=4, description='platform_x5')'''
 '''ensemble_trainer = EnsembleTrainer({
                 'line_id': 201,
                 'keep_prob' : grid_search([0.8, 0.9, 1.0]),
