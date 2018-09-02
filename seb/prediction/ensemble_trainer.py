fa3facecaa87a24d282a416264d7c872ef628d77
Sebastian Montero <smontero@mightywizards.com>
2018-09-02 21:57:45 +0900

    Added experiment

--- seb/prediction/ensemble_trainer.py
+++ seb/prediction/ensemble_trainer.py
@@ -46,6 +46,19 @@ register_trainable('car_sales_prediction_trainable', ModelTrainable)
 
 ensemble_trainer = EnsembleTrainer({
                 'line_id': 201,
+                'keep_prob' : 1.0,
+                'layer_0' : 35,
+                'max_epoch' : 2,
+                'train_months': 36,
+                'prediction_size': 1,
+                'store_window' : False,
+                'included_features' : ['inflation_index_roc_prev_month',
+                                   'consumer_confidence_index'],
+                'num_steps': 40
+            }, repeats=5, description='nationwide_ifp_new_archi_test')
+
+'''ensemble_trainer = EnsembleTrainer({
+                'line_id': 201,
                 'keep_prob' : grid_search([0.8, 0.9, 1.0]),
                 'layer_0' : grid_search([35, 40, 45, 50, 55, 60, 65]),
                 'max_epoch' : 2,
@@ -55,7 +68,7 @@ ensemble_trainer = EnsembleTrainer({
                 'included_features' : ['inflation_index_roc_prev_month',
                                    'consumer_confidence_index'],
                 'num_steps': 40
-            }, repeats=3, description='nationwide_ifp_new_archi')
+            }, repeats=3, description='nationwide_ifp_new_archi')'''
 
 '''ensemble_trainer = EnsembleTrainer({
                 'line_id': 102,
