84d4ebfae6bf267f05077ccdc845f2a6db5f8b9f
Sebastian Montero <smontero@mightywizards.com>
2018-09-04 18:32:47 +0900

    Adapted evaluator

--- seb/prediction/evaluator.py
+++ seb/prediction/evaluator.py
@@ -25,9 +25,7 @@ class Evaluator(BaseEvaluator):
         
         if len(state['_predictions'].shape) == 1:
             state['_predictions'] = np.reshape(state['_predictions'],(-1,1))
-            state['_unscaled_predictions'] = np.reshape(state['_predictions'],(-1,1))
-            print(state['_predictions'])
-            print(state['_unscaled_predictions'])
+            state['_unscaled_predictions'] = np.reshape(state['_unscaled_predictions'],(-1,1))
             
         self.__dict__.update(state)
     
