13152f52c24d536861535908a031e67bd462dc25
Sebastian Montero <smontero@mightywizards.com>
2018-09-07 22:06:28 +0900

    Added ensemble experiment

--- seb/prediction/ensemble_trainer.py
+++ seb/prediction/ensemble_trainer.py
@@ -53,7 +53,7 @@ ensemble_trainer = EnsembleTrainer({
                 'store_window' : False,
                 'included_features' : ['energy_price_index_roc_prev_month'],
                 'num_steps': 50
-            }, repeats=3, description='model_sf_ifp_2m')
+            }, repeats=3, description='model_sf_ifp_3m')
 
 '''ensemble_trainer = EnsembleTrainer({
                 'line_id': 13,
