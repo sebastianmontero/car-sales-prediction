ae29773cc6f5222e04adc1c1eca889546479ac79
Sebastian <sebastian.montero@gmail.com>
2018-09-21 17:54:28 +0900

    Added experiment

--- seb/prediction/ensemble_evolver_experiment_runner.py
+++ seb/prediction/ensemble_evolver_experiment_runner.py
@@ -12,7 +12,29 @@ ray.init()
 
 register_trainable('ensemble_evolver_trainable', EnsembleEvolverTrainable)
 
+
 run_experiments({
+    'evolver_model_2n_r5_focus' : {
+            'run': 'ensemble_evolver_trainable',
+            'trial_resources': {'cpu': 8, 'gpu': 0},
+            'stop': {'training_iteration': 70},
+            'config' : {
+                'run_path': '/home/nishilab/Documents/python/model-storage/ensemble-run-model_2n_r5-20180920234425217425',
+                'gens_per_step' : 10,
+                'indpb': 0.05,
+                'cxpb': 0.7,
+                'mutpb':  0.5,
+                'num_best': 20,
+                'tournament_size': 80,
+                'population_size': 5000,
+                'weight_range': 100,
+                'zero_percentage': 50
+            },
+            'repeat':5
+    }
+})
+
+'''run_experiments({
     'evolver_platform_1n_focus' : {
             'run': 'ensemble_evolver_trainable',
             'trial_resources': {'cpu': 8, 'gpu': 0},
@@ -31,7 +53,7 @@ run_experiments({
             },
             'repeat':5
     }
-})
+})'''
 '''run_experiments({
     'evolver_platform_3n_r4_focus' : {
             'run': 'ensemble_evolver_trainable',
